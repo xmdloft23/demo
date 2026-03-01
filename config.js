6c2922943fe3aae40e64ca9f2d54bb1b1d78d27a
Maurice Gift <founder@giftedtech.co.ke>
2026-03-02 01:42:52 +0300

    ATASSA MD

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
     TIME_ZONE: process.env.TIME_ZONE,
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS,
     AUTO_LIKE_STATUS: process.env.AUTO_LIKE_STATUS,
-    DATABASE_URL: process.env.DATABASE_URL, // Postress(Get one for free from neon.tech/supabase/render/heroku builtin postgress)...will fallback to path: ./gift/database/database.db if not provided
+    DATABASE_URL: process.env.DATABASE_URL, // Postress(Get one for free from neon.tech/supabase/render/heroku in built postgress)...will fallback to path: ./gift/database/database.db if not provided
 };
 
 let fileName = require.resolve(__filename);
