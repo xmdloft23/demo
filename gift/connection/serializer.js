ead3ffd8d75c0c4243a0971f233a225da25d5eee
xmdloft23 <xmdloft745@gmail.com>
2026-03-30 10:38:26 +0300

    Update serializer.js

--- gift/connection/serializer.js
+++ gift/connection/serializer.js
@@ -148,7 +148,7 @@ const serializeMessage = async (ms, Gifted, settings = {}) => {
         command,
         args,
         q: args.join(' '),
-        pushName: ms.pushName || (ms.key.fromMe ? Gifted.user?.name : null) || '𝐀𝐓𝐀𝐒𝐒𝐀-𝐌𝐃 User',
+        pushName: ms.pushName || (ms.key.fromMe ? Gifted.user?.name : null) || 'LOFT-QUANTUM User',
         quoted,
         repliedMessage,
         mentionedJid,
